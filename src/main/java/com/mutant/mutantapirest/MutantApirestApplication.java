65c9c52bf675521bc316ad7c75de3af10c377cdc
Johann Sebastian Torres Angarita <joahnn.1996@gmail.com>
2021-04-03 14:53:38 -0500

    Primer commit

---
+++ src/main/java/com/mutant/mutantapirest/MutantApirestApplication.java
@@ -0,0 +1,13 @@
+package com.mutant.mutantapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutantApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mutant/mutantapirest/MutantApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutant.mutantapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
